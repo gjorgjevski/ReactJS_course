29cefb5ad557341b0aa1963c9b89876d42205637
Momir Gjorgjevski <35025012+gjorgjevski@users.noreply.github.com>
2018-11-22 14:00:01 +0100

    Create Dashboard.js

---
+++ src/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component{
+render(){
+    return(
+        <div>
+            <h2>DASHBOARD DASHBOARD!!!</h2>
+            <h2>DASHBOARD DASHBOARD!!!</h2>
+        </div>
+    )
+}
+
+}
+
+export default Dashboard;
